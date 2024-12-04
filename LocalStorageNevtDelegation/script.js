00da49e9ae461bf757448380b4a2c9536b512209
samuelldmj <samuelldmj5@gmail.com>
2024-12-04 08:22:18 +0100

    day 15 in progress

---
+++ LocalStorageNevtDelegation/script.js
@@ -0,0 +1,10 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+//1 Add event listner
+function addItem(e){
+    console.log('hello');
+}
+
+addItems.addEventListener('submit', addItem)
