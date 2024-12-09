67ea5ee113ba67f77cbdb61468481f2db1694c09
samuelldmj <samuelldmj5@gmail.com>
2024-12-09 13:31:36 +0100

    sorting without articles. project 17 completed

--- sortingBandNames/script.js
+++ sortingBandNames/script.js
@@ -1,2 +1,23 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled',
+     'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+const ULElement = document.getElementById('bands');
+
+// Function to remove leading articles
+function removeArticles(str) {
+    const articles = /^(a|an|the)\s+/i;
+    return str.replace(articles, '');
+  }
+  
+  // Sort the bands while ignoring leading articles
+  const sortedBands = bands.slice().sort((a, b) => {
+    const aWithoutArticle = removeArticles(a);
+    const bWithoutArticle = removeArticles(b);
+    return aWithoutArticle.localeCompare(bWithoutArticle); 
+  });
+  
+  sortedBands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    ULElement.appendChild(li);
+  });
