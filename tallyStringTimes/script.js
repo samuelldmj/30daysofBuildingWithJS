a5a76cadec8e3bbc3d2bf2c93d0df60ed6eb430b
samuelldmj <samuelldmj5@gmail.com>
2024-12-10 13:08:55 +0100

    project 18 completed.  using reduce method to sum the video

--- tallyStringTimes/script.js
+++ tallyStringTimes/script.js
@@ -1,1 +1,31 @@
-console.log('Hello');
+//get the minutes data from the list element and sum them up.
+
+const videosClass = [...document.querySelectorAll('[data-time]')];
+
+const seconds = videosClass.map(node => node.dataset.time).map(timeNodes =>
+    {
+        const [min, sec] = timeNodes.split(':').map(parseFloat);
+        return (min * 60) + sec; 
+    }).reduce((total, vidSec) => total + vidSec);
+
+    console.log(seconds)
+
+    // let secondsLeft = seconds;
+    // const hours = Math.floor(secondsLeft / 3600);
+    // secondsLeft = secondsLeft % 3600; 
+
+    // const minutes = Math.floor(secondsLeft / 60);
+    // secondsLeft = secondsLeft % 60; 
+
+    // console.log(hours, minutes);
+
+
+    const totalSeconds = videosClass.reduce((total, node) => {
+        const [min, sec] = node.dataset.time.split(':').map(parseFloat);
+        return total + (min * 60) + sec; 
+    }, 0); // Start with 0 seconds
+    console.log(totalSeconds);
+    
+    
+
+
