2a563eeb105ba97be70bff6f60a4fdda070105f4
samuelldmj <samuelldmj5@gmail.com>
2024-12-23 14:23:08 +0100

    follow along using event delegation

--- followAlong/script.js
+++ followAlong/script.js
@@ -1,23 +1,49 @@
 const triggers = document.querySelectorAll('a');
 const highlight = document.createElement('span');
 
+// highlight.classList.add('highlight');
+// document.body.append(highlight);
+
+// function highlightLink() {
+//     const linkCoords = this.getBoundingClientRect();
+//     console.log(linkCoords);
+
+//     const coords = {
+//         width: linkCoords.width,
+//         height: linkCoords.height,
+//         top: linkCoords.top + window.scrollY, 
+//         left: linkCoords.left + window.scrollX 
+//     };
+
+//     highlight.style.width = `${coords.width}px`;
+//     highlight.style.height = `${coords.height}px`;
+//     highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+// }
+
+// triggers.forEach(aElement => aElement.addEventListener('mouseenter', highlightLink));
+
+
+//using delegation
 highlight.classList.add('highlight');
 document.body.append(highlight);
 
-function highlightLink() {
-    const linkCoords = this.getBoundingClientRect();
-    console.log(linkCoords);
+function highlightLink(e) {
+    
+    if (e.target.tagName.toLowerCase() === 'a') {
+        const linkCoords = e.target.getBoundingClientRect();
+        console.log(linkCoords);
 
-    const coords = {
-        width: linkCoords.width,
-        height: linkCoords.height,
-        top: linkCoords.top + window.scrollY, 
-        left: linkCoords.left + window.scrollX 
-    };
+        const coords = {
+            width: linkCoords.width,
+            height: linkCoords.height,
+            top: linkCoords.top + window.scrollY, 
+            left: linkCoords.left + window.scrollX 
+        };
 
-    highlight.style.width = `${coords.width}px`;
-    highlight.style.height = `${coords.height}px`;
-    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+        highlight.style.width = `${coords.width}px`;
+        highlight.style.height = `${coords.height}px`;
+        highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+    }
 }
 
-triggers.forEach(aElement => aElement.addEventListener('mouseenter', highlightLink));
+document.body.addEventListener('mouseenter', highlightLink, true);
